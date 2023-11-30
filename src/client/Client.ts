40eac88227fbfa378004dd8723c178a534f6e05a
KanakaNakazawa <tgigroupid@gmail.com>
2023-11-30 12:19:53 +0700

    Deploy Pertama

---
+++ src/client/Client.ts
@@ -0,0 +1,30 @@
+import {
+  EventEmitter
+} from "node:events";
+import clc from "cli-color";
+import WebSocket from "ws"
+
+export class Client extends EventEmitter {
+  private token: string;
+  private intents: number;
+
+  public constructor(option: ClientOption) {
+    super()
+    this.token = option?.token
+    this.intents = option?.intents
+  }
+
+  login(token: string): void {
+    if (this?.token == null) {
+      if (!token) throw Error("Token invalid")
+      this.token = token
+      console.log("Wellcome to", clc.bold("lumined.js"))
+      this.token = token
+    }
+  }
+}
+
+export type ClientOption = {
+  token: string,
+  intents: number
+}
